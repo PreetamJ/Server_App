1756a998e894a8b3549bfb1cbda603b160ab8b8b
Preetam Jahagiradar <jahagiradar.preetam@gmail.com>
2017-07-25 19:00:18 -0400

    Methods have been modified to retrieve stored document in readAndStore method instead of pulling 0th record.

--- src/main/java/com/poc/controller/POCController.java
+++ src/main/java/com/poc/controller/POCController.java
@@ -1,6 +1,7 @@
 package com.poc.controller;
 
 import java.io.File;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -14,14 +15,29 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.poc.pojo.HierarchyTest;
+
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import com.mongodb.Mongo;
 
 @RestController
 public class POCController {
+	String classID;
 
 	@Autowired
 	HierarchyRepository hierarchyRepository;
 
+	/**
+	 * This method reads and stored Hierarchy data into DB Returns message
+	 * whether JSON stored successfully or not. This method can be accessed
+	 * from client application with url "/readAndStore"
+	 * 
+	 * @param httpSession
+	 * @return msg
+	 */
 	@CrossOrigin
 	@RequestMapping("/readAndStore")
 	public String readAndStore(HttpSession httpSession) {
@@ -30,8 +46,8 @@ public class POCController {
 		String msg = "";
 		try {
 
-			String filepath="D:\\PreetamProj\\EclipseProj\\POC\\src\\main\\java\\com\\poc\\controller\\HierarchyTest.json";
-			
+			String filepath = "D:\\PreetamProj\\All Projects\\Server_App\\src\\main\\java\\com\\poc\\controller\\HierarchyTest.json";
+
 			File jsonFile = new File(filepath);
 
 			HierarchyTest hierarchyTest = new HierarchyTest();
@@ -41,7 +57,10 @@ public class POCController {
 
 			System.out.println("Id is : " + hierarchyTest.getId());
 
+			classID = hierarchyTest.getId();
+			
 			hierarchyRepository.save(hierarchyTest);
+
 			msg = "JSON file HierarchyTest has been read successfully !!!";
 
 		} catch (JsonParseException e) {
@@ -58,11 +77,23 @@ public class POCController {
 		return msg;
 	}
 
+	/**
+	 * This method retrieves data from Hierarchy collection from MongoDB
+	 * This method can be accessed from client application with url "/showTreeMap"
+	 * 
+	 * @return HierarchyTest record inserted
+	 */
 	@CrossOrigin
 	@RequestMapping(value = "/showTreeMap", produces = "application/json")
 	public HierarchyTest showTreeMap() {
 		System.out.println("Entering showTreeMap");
-		return hierarchyRepository.findAll().get(0);
+
+		HierarchyTest p = new HierarchyTest();
+		MongoOperations mongoOps = new MongoTemplate(new SimpleMongoDbFactory(new Mongo(), "poc_db"));
+		p = mongoOps.findById(classID, HierarchyTest.class);
+		System.out.println("classId is :"+classID);
+
+		return p;
 
 	}
 
